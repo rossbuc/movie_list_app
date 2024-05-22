7614ac1c0b8f41615a30abce33879df6b4581a57
Ross Buchan <rossbuchan1@googlemail.com>
2024-05-22 23:15:47 +0100

    Add some fetch functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
+  fetchData();
   runApp(const MyApp());
+}
+
+void fetchData() async {
+  final url = Uri.parse(
+      'https://movie-database-alternative.p.rapidapi.com/?s=Avengers%20Endgame');
+  print(url);
+  final response = await http.get(url, headers: {
+    'X-RapidAPI-Key': 'baa216fbefmsh1eb2e559e9272a7p13e0a8jsnf9079f31be9f',
+    'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
+  });
+
+  if (response.statusCode == 200) {
+    var data = jsonDecode(response.body);
+    print(data);
+  } else {
+    print("error in loading data, status code: ${response.statusCode}");
+    throw Exception("Failed to load data");
+  }
 }
 
 class MyApp extends StatelessWidget {
