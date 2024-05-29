c87c2e2ad55c18541db06c7812e57d2e30947b1c
Ross Buchan <rossbuchan1@googlemail.com>
2024-05-29 16:52:04 +0100

    Add background image to movie information page

--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,8 @@ class _MyHomePageState extends State<MyHomePage> {
                     var movie = value.appData[index];
                     var posterUrl =
                         'https://image.tmdb.org/t/p/w500${movie['poster_path']}';
+                    var backDropUrl =
+                        'https://image.tmdb.org/t/p/w500${movie['backdrop_path']}';
 
                     return ListTile(
                       leading: Image.network(
@@ -70,16 +72,8 @@ class _MyHomePageState extends State<MyHomePage> {
                       onTap: () {
                         Navigator.push(context,
                             MaterialPageRoute(builder: (BuildContext context) {
-                          return Scaffold(
-                            appBar: AppBar(
-                                title: Text("${movie["original_title"]}")),
-                            body: Center(
-                              child: Hero(
-                                tag: "ListTile-Hero",
-                                child: Text("${movie["overview"]}"),
-                              ),
-                            ),
-                          );
+                          return MovieDetailPage(
+                              backDropUrl: backDropUrl, movie: movie);
                         }));
                       },
                     );
@@ -92,5 +86,47 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ));
+  }
+}
+
+class MovieDetailPage extends StatelessWidget {
+  const MovieDetailPage({
+    super.key,
+    required this.backDropUrl,
+    required this.movie,
+  });
+
+  final String backDropUrl;
+  final movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: Image.network(
+            backDropUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: Text("${movie["original_title"]}"),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+          body: Center(
+            child: Hero(
+              tag: "ListTile-Hero",
+              child: Text(
+                "${movie["overview"]}",
+                style: const TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
