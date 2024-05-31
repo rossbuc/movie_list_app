5de61dd5583f74a556442b59389f24f4bf1ba02a
Ross Buchan <rossbuchan1@googlemail.com>
2024-05-31 11:09:21 +0100

    Update styling on ovie detail page

--- lib/movie_detail_page.dart
+++ lib/movie_detail_page.dart
@@ -8,36 +8,71 @@ class MovieDetailPage extends StatelessWidget {
   });
 
   final String backDropUrl;
-  final movie;
+  final Map<String, dynamic> movie;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: Image.network(
-            backDropUrl,
-            fit: BoxFit.cover,
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.network(
+              backDropUrl,
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            title: Text("${movie["original_title"]}"),
-            backgroundColor: Colors.transparent,
-            elevation: 0,
+          Positioned.fill(
+            child: Container(
+              color: Colors.black.withOpacity(0.5), // Adjust opacity as needed
+            ),
           ),
-          body: Center(
-            child: Hero(
-              tag: "ListTile-Hero",
-              child: Text(
-                "${movie["overview"]}",
-                style: const TextStyle(color: Colors.white, fontSize: 18),
+          SafeArea(
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                title: Text(
+                  "${movie["original_title"]}",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    shadows: [
+                      Shadow(
+                        offset: Offset(1.0, 1.0),
+                        blurRadius: 3.0,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ],
+                  ),
+                ),
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+              ),
+              body: Center(
+                child: Hero(
+                  tag: "ListTile-Hero",
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      "${movie["overview"]}",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        shadows: [
+                          Shadow(
+                            offset: Offset(1.0, 1.0),
+                            blurRadius: 3.0,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
